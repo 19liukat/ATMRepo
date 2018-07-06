d12600b12e75d8c9f3c369d59293f02171a43826
Unknown <19liukat@elmbrookstudents.org>
2018-07-05 22:53:40 -0500

    all files

---
+++ ATMFinal/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace ATM
+{
+    class ATMProgram
+    {
+        private int hundred;
+        private int fifty;
+        private int twenty;
+        private int ten;
+        private int five;
+        private int one;
+
+        public ATMProgram()
+        {
+            hundred = 10;
+            fifty = 10;
+            twenty = 10;
+            ten = 10;
+            five = 10;
+            one = 10;
+        }
+
+        public void status()
+        {
+            Console.WriteLine("Machine Status: \n$100 - " + hundred + "\n$50 - " + fifty + "\n$20 - " + twenty + "\n$10 - "
+                     + ten + "\n$5 - " + five + "\n$1 - " + one);
+        }
+
+        public void restock()
+        {
+            hundred = 10;
+            fifty = 10;
+            twenty = 10;
+            ten = 10;
+            five = 10;
+            one = 10;
+            this.status();
+        }
+
+        public void withdraw(int amount)
+        {
+            int total = hundred * 100 + fifty * 50 + twenty * 20 + ten * 10 + five * 5 + one;
+            if (amount > total)
+            {
+                Console.WriteLine("Failure: Insufficient funds");
+                return;
+            }
+            if (amount % 5 > one)
+            {
+                Console.WriteLine("Failure: Insufficient funds");
+                return;
+            }
+            // 100
+            Console.WriteLine("Success: Withdrew $" + amount);
+            int numHundred = amount / 100;
+            if (hundred >= numHundred)
+            {
+                hundred -= numHundred;
+            }
+            else
+            {
+                numHundred = hundred;
+                hundred = 0;
+            }
+            amount -= (numHundred * 100);
+            // 50
+            int numFifty = amount / 50;
+            if (fifty >= numFifty)
+            {
+                fifty -= numFifty;
+            }
+            else
+            {
+                numFifty = fifty;
+                fifty = 0;
+            }
+            amount -= (numFifty * 50);
+            // 20
+            int numTwenty = amount / 20;
+            if (twenty >= numTwenty)
+            {
+                twenty -= numTwenty;
+            }
+            else
+            {
+                numTwenty = twenty;
+                twenty = 0;
+            }
+            amount -= (numTwenty * 20);
+            // 10
+            int numTen = amount / 10;
+            if (ten >= numTen)
+            {
+                ten -= numTen;
+            }
+            else
+            {
+                numTen = ten;
+                ten = 0;
+            }
+            amount -= (numTen * 10);
+            // 5
+            int numFive = amount / 5;
+            if (five >= numFive)
+            {
+                five -= numFive;
+            }
+            else
+            {
+                numFive = five;
+                five = 0;
+            }
+            amount -= (numFive * 5);
+            // 1
+            int numOne = amount;
+            if (one >= numOne)
+            {
+                one -= numOne;
+            }
+            else
+            {
+                Console.WriteLine("Failure: Insufficient funds");
+            }
+            this.status();
+        }
+
+        public int getHundreds()
+        {
+            return hundred;
+        }
+
+        public int getFifty()
+        {
+            return fifty;
+        }
+
+        public int getTwenty()
+        {
+            return twenty;
+        }
+
+        public int getTens()
+        {
+            return ten;
+        }
+
+        public int getFives()
+        {
+            return five;
+        }
+
+        public int getOnes()
+        {
+            return one;
+        }
+
+
+
+
+    }
+}
---
+++ ATMFinal/Test.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM
+{
+    class Test
+    {
+        public static void Main(String[] args)
+        {
+            ATMProgram bob = new ATMProgram();
+            int quit = 1;
+            do
+            {
+                String directions = Console.ReadLine();
+                int index = -999;
+                int value = -999;
+                String directionsPlusSpace = directions + " ";
+                char first = char.ToLower(directions[0]);
+                switch (first)
+                {
+                    case 'r':
+                        bob.restock();
+                        break;
+                    case 'w':
+                        index = directions.IndexOf("$");
+                        value = Int32.Parse(directions.Substring(index + 1));
+                        bob.withdraw(value);
+                        break;
+                    case 'i':
+                        if (directionsPlusSpace.Contains(" $100 "))
+                        {
+                            Console.WriteLine("$100 - " + bob.getHundreds());
+                        }
+                        if (directionsPlusSpace.Contains(" $50 "))
+                        {
+                            Console.WriteLine("$50 - " + bob.getFifty());
+                        }
+                        if (directionsPlusSpace.Contains(" $20 "))
+                        {
+                            Console.WriteLine("$20 - " + bob.getTwenty());
+                        }
+                        if (directionsPlusSpace.Contains(" $10 "))
+                        {
+                            Console.WriteLine("$10 - " + bob.getTens());
+                        }
+                        if (directionsPlusSpace.Contains(" $5 "))
+                        {
+                            Console.WriteLine("$5 - " + bob.getFives());
+                        }
+                        if (directionsPlusSpace.Contains(" $1 "))
+                        {
+                            Console.WriteLine("$1 - " + bob.getOnes());
+                        }
+                        break;
+                    case 'q':
+                        quit = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Failure: Invalid Command");
+                        break;
+
+
+                }
+
+            } while (quit != 0);
+
+
+            Console.ReadKey();
+        }
+    }
+}
